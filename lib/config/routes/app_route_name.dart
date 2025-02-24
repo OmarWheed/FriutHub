50c024a3ccad746143ac4c19f35e609c7d375ae7
omar wheed <wheedomar88@gmail.com>
2025-02-24 18:57:58 +0200

    splash view done

---
+++ lib/config/routes/app_route_name.dart
@@ -0,0 +1,4 @@
+class AppRouteName {
+  static const String splashView = "SPLASH_VIEW";
+  static const String onBoarding = "ON_BOARDING";
+}
---
+++ lib/core/utils/app_assets_name.dart
@@ -0,0 +1,5 @@
+class AppImageName {
+  static const String splashLogo = "assets/images/splash_logo.svg";
+  static const String splashCircles = "assets/images/splash_circles.svg";
+  static const String splashPlant = "assets/images/splash_plant.svg";
+}
